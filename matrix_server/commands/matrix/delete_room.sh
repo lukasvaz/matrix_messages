90922d906a7a93cc2b50c149834ad513bea1992c
lukasvaz <lukas.vasquez.verdejo@gmail.com>
2026-03-06 21:17:13 -0300

    chore: allow root user  to publish room in directory

--- matrix_server/commands/matrix/delete_room.sh
+++ matrix_server/commands/matrix/delete_room.sh
@@ -6,8 +6,6 @@ set -e
 prompt() { printf '%s' "$1" > /dev/tty; read ans < /dev/tty; printf '%s' "$ans"; }
 room_id=$(prompt "Room id to delete (eg: !abc:domain or #alias:domain): ")
 if [ -z "$room_id" ]; then printf 'No room id provided, aborting.\n' > /dev/tty; exit 1; fi
-confirm=$(prompt "Permanently purge room ${room_id}? type 'yes' to confirm: ")
-if [ "$confirm" != "yes" ]; then printf 'Aborted.\n' > /dev/tty; exit 1; fi
 payload='{"purge":true}'
 resp=$(api_delete_json "/_synapse/admin/v2/rooms/${room_id}" "$payload")
 print_json "$resp"
